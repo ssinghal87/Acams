778f1f551be19eb8e6b9d5b4a32dac095f1e64b0
ACROTECHNOLOGIE\SSinghal <sarthak.singhal8723@gmail.com>
2018-01-20 07:38:44 -0500

    Signed-off-by: ACROTECHNOLOGIE\SSinghal <sarthak.singhal8723@gmail.com>

---
+++ Acro_Data_Driven_Framework/src/test/java/com/acams/ddf/testcases/rought.java
@@ -0,0 +1,19 @@
+package com.acams.ddf.testcases;
+
+import org.testng.annotations.Test;
+
+import com.acams.ddf.base.BaseTest;
+
+public class rought extends BaseTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+		
+	
+	}
+	
+	
+
+}
